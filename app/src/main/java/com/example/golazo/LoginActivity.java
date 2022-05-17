add2a4b1dcc8965f3ec4fcabffb48476ab548193
Nedim Bandžović <nedim.bandzovic2001@gmail.com>
2022-05-17 15:53:15 +0200

    Add login screen and splash screen

---
+++ app/src/main/java/com/example/golazo/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.golazo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+        signUp=findViewById(R.id.textView4);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/golazo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.golazo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        final Handler handler=new Handler();
+        final Runnable r = new Runnable() {
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+            }
+        };
+
+        handler.postDelayed(r, 4000);
+    }
+}
