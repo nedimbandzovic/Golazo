959918170f6eef86b6d73830ea8cd1e7709b7160
Nedim Bandžović <nedim.bandzovic2001@gmail.com>
2022-05-17 18:26:26 +0200

    Login logic implementation

--- app/src/main/java/com/example/golazo/LoginActivity.java
+++ app/src/main/java/com/example/golazo/LoginActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     TextView signUp;
+    EditText username, password;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,39 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        button=findViewById(R.id.loginbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernameText=username.getText().toString();
+                String passwordText=password.getText().toString();
+                if (usernameText.isEmpty()||passwordText.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Fill in both fields", Toast.LENGTH_SHORT).show();
+                } else{
+                    UserDatabase userDatabase=UserDatabase.getUserDatabase(getApplicationContext());
+                    UserDao userDao=userDatabase.userDao();
+                    new Thread(new Runnable(){
+                        @Override
+                        public void run(){
+                            User user= userDao.login(usernameText, passwordText);
+                            if (user == null){
+                                runOnUiThread(new Runnable(){@Override
+                                    public void run() {
+                                        Toast.makeText(getApplicationContext(), "Entered credentials are not correct, try again", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            } else{
+                                Intent intent=new Intent (LoginActivity.this, WelcomeScreen.class);
+                                startActivity(intent);
+                            }
+                        }
+                    }).start();
+                }
+            }
+        });
+
 
 
     }
